1438bb01cda41347ee279fe175169209da1d4576
MS_btff <whdghks845@gmail.com>
2023-05-19 12:58:01 +0900

    react_pr1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+//props 객체에서 키와 값으로 이루어진 파일 포맷을 얻는데
+// 리액트에 의해 자동으로 전달됨
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>23/ 5/ 18</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
